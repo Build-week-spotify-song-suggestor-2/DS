fe19f270a296e95b86e926d4fe2f945a6dd7b57a
Adam Carter <adm.carter@gmail.com>
2020-10-22 18:28:02 -0400

    Add viz endpoint

--- app/api/predict.py
+++ app/api/predict.py
@@ -8,7 +8,7 @@ import pandas as pd
 from pydantic import BaseModel, Field, validator
 
 from app.api.client import client
-from app.api.recommend import find_recommended_songs, track_id_in_df
+from app.api.recommend import find_recommended_songs, track_id_in_df, get_radar_plot
 
 log = logging.getLogger(__name__)
 router = APIRouter()
@@ -16,7 +16,7 @@ router = APIRouter()
 from .. import crud, models, schemas
 from ..database import SessionLocal, engine
 
-# models.Base.metadata.drop_all(bind=engine)
+models.Base.metadata.drop_all(bind=engine)
 models.Base.metadata.create_all(bind=engine)
 
 # Dependency
@@ -62,17 +62,19 @@ async def predict(track: schemas.TrackCreate, db: Session = Depends(get_db)):
             db_track
           )
 
-    suggestions = crud.get_suggestions(db, track_id)
-
-    return {"recommendations": list(
-      zip((s.title for s in suggestions),
-         ((a.name for a in s.artists) for s in suggestions))
-    )}
+    return {
+      "recommendations": [
+        {
+          "title": s.title,
+          "artists": [a.name for a in s.artists]
+        }
+        for s in crud.get_suggestions(db, track_id)
+      ]}
 
 
 def find_track(db, track_id):
   track = crud.get_track(db, track_id)
-  if track: return
+  if track: return track
 
   title, artist_names = client.request_track_info(track_id)
 
@@ -85,3 +87,33 @@ def find_track(db, track_id):
     track = crud.add_artist_to_track(db, artist=artist, track=track)
 
   return track
+
+
+@router.post('/viz') # , response_model=List[schemas.Track])
+async def viz(track: schemas.TrackCreate, db: Session = Depends(get_db)):
+    """
+    Suggest a list of recommendations for the specified Track.
+
+    ### Request Body
+    - `title`: string
+    - `artist`: string
+    """
+    track_ids = client.request_track_ids(track.title, track.artist)
+    for track_id in track_ids:
+      db_track = find_track(db, track_id)
+      if db_track: break
+    else:
+      return {"error": f"{track.title} by {track.artist} not found."}
+
+    if not db_track.suggestions:  
+      for rank, suggested_track_id in enumerate(find_recommended_songs(db_track.id)):
+        suggested_track = find_track(db, suggested_track_id)
+
+        if suggested_track:
+          crud.add_suggestion_to_track(
+            db,
+            crud.create_suggestion(db, suggested_track.id, rank),
+            db_track
+          )
+
+    return get_radar_plot([s.id for s in crud.get_suggestions(db, track_id)])
--- app/api/recommend.py
+++ app/api/recommend.py
@@ -2,6 +2,22 @@ import os
 import pickle
 import pandas
 
+import plotly.graph_objects as go
+import chart_studio.plotly as py
+from chart_studio.tools import set_credentials_file
+from dotenv import load_dotenv
+load_dotenv()
+
+PLOTLY_USERNAME = os.getenv("PLOTLY_USERNAME")
+PLOTLY_API_KEY = os.getenv("PLOTLY_API_KEY")
+
+chart_id = 0
+
+set_credentials_file(
+    username=PLOTLY_USERNAME,
+    api_key=PLOTLY_API_KEY
+)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 model_path = os.path.join(basedir, '../../ml_model/recommendation_model.sav')
 df_path = os.path.join(basedir, '../../ml_model/song_dataset.pkl')
@@ -14,20 +30,55 @@ with open(df_path, 'rb') as f:
 
 
 def find_recommended_songs(track_id):
-    # from track_id to audio_features
     song_index = df.index[df['id'] == track_id]
     audio_features = df.iloc[song_index, 3:].to_numpy()
-    # recommendation model
-    distances, indices = model.kneighbors(audio_features) #.reshape(1, -1)
+    indices = model.kneighbors(audio_features)[1]
     return df.loc[indices[0], 'id'].tolist()
-  
-    # how to search song names from ids
-    # on the ML side
-    # recommended_song_list = []
-    # for recommend in recommended_list:
-    #     recommend_song_name=df[df['id']==recommend]
-    #     recommended_song_list.append(recommend_song_name)
-    # return recommended_song_list
+
 
 def track_id_in_df(track_id):
   return len(df.index[df['id'] == track_id]) > 0
+
+
+def get_radar_plot(track_ids):
+    """
+      Create combined radar plots of the recommended songs for the given track id.
+    """
+    global chart_id
+    songs = [df[df["id"] == id] for id in track_ids]
+
+    fig = go.Figure()
+    audio_features = ["acousticness", "danceability", "energy", 
+                      "liveness",  "speechiness",  "valence"]
+
+    for song in songs:
+      fig.add_trace(go.Scatterpolar(
+        r=[float(song[feature]) for feature in audio_features],
+        theta=audio_features,
+        fill="toself",
+        name="Product A",
+        opacity=0.9
+      ))
+
+    fig.update_layout(
+      polar=dict(
+        radialaxis=dict(
+          visible=True,
+          range=[0, 0.7]
+        )),
+      showlegend=False,
+      title = "10 recommended songs from AutoEncoder - Neural Network Model"
+    )
+
+
+    chart = py.plot(
+        fig,
+        filename=f"track_id_{chart_id}",
+        auto_open=False,
+        fileopt='overwrite',
+        sharing='public'
+    )
+    
+    chart_id += 1
+
+    return chart
